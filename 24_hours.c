909a815b1812987cd4e85cbf39b694f4f35837a2
sarerrdy <sarerrdy4live@gmail.com>
2024-03-26 12:26:01 +0100

    24 hour clock

---
+++ 24_hours.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* jack_bauer - Print every minute of the day of Jack Bauer,
+* starting from 00:00 to 23:59
+*/
+void main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 12; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(':');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			putchar('\n');
+		}
+	}
+}
