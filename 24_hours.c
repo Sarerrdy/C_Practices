455266c1833a9a5ccc85938f6b22737b9f647d78
sarerrdy <sarerrdy4live@gmail.com>
2024-03-26 13:33:47 +0100

    multiplication table

--- 24_hours.c
+++ 24_hours.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
-* jack_bauer - Print every minute of the day of Jack Bauer,
+* main - Print every minute of the day of Jack Bauer,
 * starting from 00:00 to 23:59
 */
+
 void main(void)
 {
 	int i, j;
---
+++ main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_FILE
+#define MAIN_FILE
+
+#include <stdio.h>
+
+void times_table(void);
+
+#endif
